ed1cb1a04b9b189384152c99d54440b9b8693518
StopStone <wltjr6432@naver.com>
2025-01-09 23:21:29 +0900

    Refactor: name 속성 변경

--- app/src/main/java/com/nemodream/bangkkujaengi/CustomerFragment.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/CustomerFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.nemodream.bangkkujaengi.databinding.FragmentCustomerBinding
 
+
 class CustomerFragment: Fragment() {
     private var _binding: FragmentCustomerBinding? = null
     private val binding get() = _binding!!
