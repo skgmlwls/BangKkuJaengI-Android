970d5576afe082239c26b4cea7ab269b7120fa49
stopstone <wltjr6432@naver.com>
2025-01-12 16:08:48 +0900

    Feat: 상품 정보 받아오기

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/data/repository/HomeRepository.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/data/repository/HomeRepository.kt
@@ -4,6 +4,7 @@ import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.nemodream.bangkkujaengi.customer.data.model.Banner
+import com.nemodream.bangkkujaengi.customer.data.model.Product
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -21,18 +22,27 @@ class HomeRepository @Inject constructor(
             .get()
             .await()
             .documents
-            .map { doc -> // 상대경로를 통해 storage에서 이미지 불러오기
-                doc.toBanner().copy(thumbnailImageRef = getImageUrl(doc))
+            .map { doc ->
+                doc.toBanner().copy(thumbnailImageRef = getImageUrl(doc.getString("imageUrl") ?: ""))
             }
-
     /*
     * Firebase Storage에서 상대 경로에 맞는 이미지 가져오기
     * */
-    private suspend fun getImageUrl(snapshot: DocumentSnapshot) = storage.reference
-        .child(snapshot.getString("imageUrl") ?: "")
+    private suspend fun getImageUrl(imagePath: String) = storage.reference
+        .child(imagePath)
         .downloadUrl
         .await()
         .toString()
+
+    /*
+    * productId를 통해 Firebase의 Product Collection에서 해당 상품 데이터 가져오기
+    * */
+    suspend fun getProducts(productId: String): Product? =
+        firestore.collection("Product")
+            .document(productId)
+            .get()
+            .await()
+            .toProduct()
 
     /*
     * Firebase Firestore에서 배너 데이터 가져오기
@@ -43,4 +53,23 @@ class HomeRepository @Inject constructor(
             productId = getString("productId") ?: "",
             thumbnailImageRef = getString("imageUrl") ?: ""
         )
+
+    /*
+    * Firebase Firestore에서 상품 데이터 가져오기
+    * */
+    private suspend fun DocumentSnapshot.toProduct(): Product {
+        val imageRefs = get("images") as? List<String> ?: emptyList()
+        val imageUrls = imageRefs.map { imagePath -> getImageUrl(imagePath) }
+
+        return Product(
+            productId = id,
+            productName = getString("productName") ?: "",
+            description = getString("description") ?: "",
+            images = imageUrls,
+            isBest = getBoolean("isBest") ?: false,
+            price = getLong("price")?.toInt() ?: 0,
+            productCount = getLong("productCount")?.toInt() ?: 0,
+            saledPrice = getLong("saledPrice")?.toInt() ?: 0,
+        )
+    }
 }
