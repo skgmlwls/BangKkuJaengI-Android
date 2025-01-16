b5e1b60ca5fd0c0f3f4ab5b94afe0f6b9274d12d
StopStone <wltjr6432@naver.com>
2025-01-16 10:38:24 +0900

    Fix: 백스택이 추가 안되는 현상 수정

--- app/src/main/java/com/nemodream/bangkkujaengi/AdminActivity.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/AdminActivity.kt
@@ -1,16 +1,33 @@
 package com.nemodream.bangkkujaengi
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.nemodream.bangkkujaengi.container.AdminFragment
 import com.nemodream.bangkkujaengi.databinding.ActivityAdminBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AdminActivity : AppCompatActivity() {
     private val binding: ActivityAdminBinding by lazy { ActivityAdminBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.admin_parent_container, AdminFragment()) // add에서 replace로 변경
+                .commit()
+        }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    supportFragmentManager.popBackStack()
+                } else {
+                    finish()
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/nemodream/bangkkujaengi/container/AdminFragment.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/container/AdminFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import com.nemodream.bangkkujaengi.R
+import com.nemodream.bangkkujaengi.admin.ui.fragment.AdminHomeFragment
 import com.nemodream.bangkkujaengi.databinding.FragmentAdminBinding
 
 class AdminFragment: Fragment() {
@@ -22,10 +25,27 @@ class AdminFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // AdminHomeFragment를 초기 화면으로 설정
+        if (savedInstanceState == null) {
+            childFragmentManager.beginTransaction()
+                .add(R.id.admin_container, AdminHomeFragment())
+                .commit()
+        }
+
+        // 백 네비게이션 처리
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            if (childFragmentManager.backStackEntryCount > 0) {
+                childFragmentManager.popBackStack()
+            } else {
+                requireActivity().finish()
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
