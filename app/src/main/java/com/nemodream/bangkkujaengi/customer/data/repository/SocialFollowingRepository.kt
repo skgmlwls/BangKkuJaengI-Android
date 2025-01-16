2fd6141b72a4b6b22cce97aa413aff7a5757850d
hyeinLion <hyein604@naver.com>
2025-01-16 14:19:32 +0900

    멤버 임시 데이터 추가

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/data/repository/SocialFollowingRepository.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/data/repository/SocialFollowingRepository.kt
@@ -25,9 +25,9 @@ class SocialFollowingRepository @Inject constructor(){
                 point = 3000,
                 isActive = true,
                 createAt = System.currentTimeMillis(),
-                followingCount = 5,
+                followingCount = 1,
                 followingList = emptyList(),
-                followerCount = 6
+                followerCount = 1
             ),
             Member(
                 id = "2",
@@ -40,9 +40,9 @@ class SocialFollowingRepository @Inject constructor(){
                 point = 3000,
                 isActive = true,
                 createAt = System.currentTimeMillis(),
-                followingCount = 3,
+                followingCount = 2,
                 followingList = emptyList(),
-                followerCount = 7
+                followerCount = 2
             ),
             Member(
                 id = "3",
@@ -55,10 +55,97 @@ class SocialFollowingRepository @Inject constructor(){
                 point = 3000,
                 isActive = true,
                 createAt = System.currentTimeMillis(),
+                followingCount = 3,
+                followingList = emptyList(),
+                followerCount = 3
+            ),
+            Member(
+                id = "4",
+                memberId = "user4",
+                memberPassword = "password",
+                memberName = "User Two",
+                memberNickName = "닉네임4",
+                memberPhoneNumber = "010-5678-1234",
+                memberProfileImage = "https://example.com/profile4.jpg",
+                point = 3000,
+                isActive = true,
+                createAt = System.currentTimeMillis(),
+                followingCount = 4,
+                followingList = emptyList(),
+                followerCount = 4
+            ),
+            Member(
+                id = "5",
+                memberId = "user5",
+                memberPassword = "password",
+                memberName = "User Two",
+                memberNickName = "닉네임5",
+                memberPhoneNumber = "010-5678-1234",
+                memberProfileImage = "https://example.com/profile5.jpg",
+                point = 3000,
+                isActive = true,
+                createAt = System.currentTimeMillis(),
+                followingCount = 5,
+                followingList = emptyList(),
+                followerCount = 5
+            ),
+            Member(
+                id = "6",
+                memberId = "user6",
+                memberPassword = "password",
+                memberName = "User One",
+                memberNickName = "닉네임6",
+                memberPhoneNumber = "010-1234-5678",
+                memberProfileImage = "https://example.com/profile6.jpg",
+                point = 3000,
+                isActive = true,
+                createAt = System.currentTimeMillis(),
                 followingCount = 6,
                 followingList = emptyList(),
+                followerCount = 6
+            ),Member(
+                id = "7",
+                memberId = "user7",
+                memberPassword = "password",
+                memberName = "User One",
+                memberNickName = "닉네임7",
+                memberPhoneNumber = "010-1234-5678",
+                memberProfileImage = "https://example.com/profile7.jpg",
+                point = 3000,
+                isActive = true,
+                createAt = System.currentTimeMillis(),
+                followingCount = 7,
+                followingList = emptyList(),
+                followerCount = 7
+            ),Member(
+                id = "8",
+                memberId = "user8",
+                memberPassword = "password",
+                memberName = "User One",
+                memberNickName = "닉네임8",
+                memberPhoneNumber = "010-1234-5678",
+                memberProfileImage = "https://example.com/profile8.jpg",
+                point = 3000,
+                isActive = true,
+                createAt = System.currentTimeMillis(),
+                followingCount = 8,
+                followingList = emptyList(),
                 followerCount = 8
-            )
+            ),Member(
+                id = "9",
+                memberId = "user9",
+                memberPassword = "password",
+                memberName = "User One",
+                memberNickName = "닉네임9",
+                memberPhoneNumber = "010-1234-5678",
+                memberProfileImage = "https://example.com/profile9.jpg",
+                point = 3000,
+                isActive = true,
+                createAt = System.currentTimeMillis(),
+                followingCount = 9,
+                followingList = emptyList(),
+                followerCount = 9
+            ),
         )
     }
 
