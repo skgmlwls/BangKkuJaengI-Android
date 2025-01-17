ae1e696748f549284e96daf7410c426087f4e33d
StopStone <wltjr6432@naver.com>
2025-01-17 12:17:58 +0900

    Build: Fagment 간 데이저 전달을 위한 SafeArgs 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("kotlin-parcelize")
