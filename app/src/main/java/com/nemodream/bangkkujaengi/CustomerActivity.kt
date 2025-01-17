7569a0de348abea1181144275f36a66a3859e345
StopStone <wltjr6432@naver.com>
2025-01-17 12:08:42 +0900

    Feat: NavHost 설정

--- app/src/main/java/com/nemodream/bangkkujaengi/CustomerActivity.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/CustomerActivity.kt
@@ -1,7 +1,11 @@
 package com.nemodream.bangkkujaengi
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.nemodream.bangkkujaengi.databinding.ActivityCustomerBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,5 +16,32 @@ class CustomerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val navController = setupNavHost()
+        setupListeners(navController)
+    }
+
+    // NavHost 설정
+    private fun setupNavHost(): NavController {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.customer_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.customerBottomNavigation.setupWithNavController(navController)
+        return navController
+    }
+
+    private fun setupListeners(navController: NavController) {
+        // 특정 화면에서 BottomNavigation 숨기기/보이기
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                // BottomNavigation이 보여야 되는 화면 정의
+                R.id.navigation_home, R.id.navigation_social -> {
+                    binding.customerBottomNavigation.visibility = View.VISIBLE
+                }
+
+                else -> {
+                    binding.customerBottomNavigation.visibility = View.GONE
+                }
+            }
+        }
     }
 }
