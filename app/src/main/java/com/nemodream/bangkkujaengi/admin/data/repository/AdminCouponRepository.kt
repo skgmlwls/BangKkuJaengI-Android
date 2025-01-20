9ab6c45eff80b3300fb1f02abe267314d1430d51
StopStone <wltjr6432@naver.com>
2025-01-20 13:28:23 +0900

    Feat: 쿠폰 삭제 기능 추가

--- app/src/main/java/com/nemodream/bangkkujaengi/admin/data/repository/AdminCouponRepository.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/admin/data/repository/AdminCouponRepository.kt
@@ -25,4 +25,9 @@ class AdminCouponRepository @Inject constructor(
     } catch (e: Exception) {
         throw e
     }
+
+    // 쿠폰 삭제하기
+    fun deleteCoupon(coupon: Coupon) {
+        firestore.collection("Coupon").document(coupon.documentId).delete()
+    }
 }
