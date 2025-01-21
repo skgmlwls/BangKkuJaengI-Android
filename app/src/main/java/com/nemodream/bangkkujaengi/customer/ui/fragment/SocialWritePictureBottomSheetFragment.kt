8ba1401e05e5033d712084ad65b9b51c7cb166b1
hyeinLion <hyein604@naver.com>
2025-01-21 15:11:58 +0900

    바텀시트 올리기

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/fragment/SocialWritePictureBottomSheetFragment.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/fragment/SocialWritePictureBottomSheetFragment.kt
@@ -5,45 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.nemodream.bangkkujaengi.R
-import com.nemodream.bangkkujaengi.databinding.FragmentSocialFollowingAllBinding
 import com.nemodream.bangkkujaengi.databinding.FragmentSocialWritePictureBinding
+import com.nemodream.bangkkujaengi.databinding.FragmentSocialWritePictureBottomSheetBinding
 
-class SocialWritePictureFragment : Fragment() {
-
-    private var _binding: FragmentSocialWritePictureBinding? = null
+class SocialWritePictureBottomSheetFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentSocialWritePictureBottomSheetBinding? = null
     private val binding get() = _binding!!
 
     // 프래그먼트의 뷰를 생성하는 메서드
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSocialWritePictureBinding.inflate(inflater, container, false)
+        _binding = FragmentSocialWritePictureBottomSheetBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     // 뷰가 생성된 후 초기화 작업을 수행하는 메서드
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupListeners()
+
     }
 
     // 프래그먼트가 파괴될 때 Binding 해제
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    // 리스너 설정
-    private fun setupListeners() {
-        with(binding) {
-            toolbarSocial.setNavigationOnClickListener {
-                findNavController().popBackStack(R.id.navigation_social, false)
-            }
-            btnAddPicture.setOnClickListener {
-                val socialWritePictureBottomSheetFragment = SocialWritePictureBottomSheetFragment()
-                socialWritePictureBottomSheetFragment.show(childFragmentManager, "SocialWritePictureBottomSheetFragment")
-            }
-        }
     }
 }
