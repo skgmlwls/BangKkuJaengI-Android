0599abbfaa61d1e901d21f56ea88b3b61b5557a8
hyeinLion <hyein604@naver.com>
2025-01-21 13:25:46 +0900

    백스택 오류 수정

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/fragment/SocialWritePictureFragment.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/fragment/SocialWritePictureFragment.kt
@@ -5,15 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.nemodream.bangkkujaengi.R
+import com.nemodream.bangkkujaengi.databinding.FragmentSocialFollowingAllBinding
+import com.nemodream.bangkkujaengi.databinding.FragmentSocialWritePictureBinding
 
 class SocialWritePictureFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Fragment의 레이아웃 설정
-        return inflater.inflate(R.layout.fragment_social_write_picture, container, false)
+    private var _binding: FragmentSocialWritePictureBinding? = null
+    private val binding get() = _binding!!
+
+    // 프래그먼트의 뷰를 생성하는 메서드
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSocialWritePictureBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    // 뷰가 생성된 후 초기화 작업을 수행하는 메서드
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    // 프래그먼트가 파괴될 때 Binding 해제
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    // 리스너 설정
+    private fun setupListeners() {
+        with(binding) {
+            toolbarSocial.setNavigationOnClickListener {
+                findNavController().popBackStack(R.id.navigation_social, false)
+            }
+        }
     }
 }
