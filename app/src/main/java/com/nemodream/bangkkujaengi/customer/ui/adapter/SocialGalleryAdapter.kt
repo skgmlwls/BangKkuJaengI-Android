1136facef343f44d01806cc4251de2dd48f3783a
hyeinLion <hyein604@naver.com>
2025-01-22 15:51:30 +0900

    주석 추가

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/adapter/SocialGalleryAdapter.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/adapter/SocialGalleryAdapter.kt
@@ -13,9 +13,13 @@ class SocialGalleryAdapter(private val photoList: List<Uri>) :
     inner class SocialGalleryViewHolder(private val binding: ItemSocialGalleryPhotoBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        // photoUri : 표시할 이미지의 URI
         fun bind(photoUri: Uri) {
+            // Glide 라이브러리 : 이미지를 ImageView에 비동기적으로 로드
             Glide.with(binding.root.context)
+                // photoUri를 통해 이미지를 로드
                 .load(photoUri)
+                // 이미지를 ivPhoto에 표시
                 .into(binding.ivPhoto)
 
             // 사진 클릭 이벤트
@@ -28,7 +32,9 @@ class SocialGalleryAdapter(private val photoList: List<Uri>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SocialGalleryViewHolder {
         val binding = ItemSocialGalleryPhotoBinding.inflate(
             LayoutInflater.from(parent.context),
+            // RecyclerView의 부모 뷰
             parent,
+            // 루트 뷰를 즉시 부모 뷰에 추가하지 않는다
             false
         )
         return SocialGalleryViewHolder(binding)
