f06889505ce0a3dbb73b81b794ee8ab2164aee78
hyeinLion <hyein604@naver.com>
2025-01-27 22:37:17 +0900

    태그 데이터 모델에 pinView 항목 삭제

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/data/model/Tag.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/data/model/Tag.kt
@@ -1,11 +1,10 @@
 package com.nemodream.bangkkujaengi.customer.data.model
 
-import android.widget.ImageView
 
 data class Tag(
     val order: Int,
     val tagX: Float,
     val tagY: Float,
     val tagProductInfo: Product,
-    var pinView: ImageView? = null // 태그 핀의 UI 요소
+
 )
