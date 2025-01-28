88efc388269018fb29673bf1ee3f4514e916a14c
hyeinLion <hyein604@naver.com>
2025-01-29 00:00:32 +0900

    Remove: 게시글 썸네일

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/data/model/Post.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/data/model/Post.kt
@@ -5,34 +5,22 @@ import android.nfc.Tag
 data class Post(
     // 게시글 고유 아이디
     val id: String = "",
-
     // 작성자 아이디
     val nickname: String = "",
-
     // 작성자 프사
     val authorProfilePicture: String = "",
-
     // 게시글 제목
     val title: String = "",
-
     // 게시글 내용
     val content: String = "",
-
     // 게시 사진들
     val imageList: List<String> = emptyList(),
-
-    // 게시글 썸네일
-    // val thumbnailImage: imageList[0]
-
-     // 사진 위 태그들
-     val productTagPinList : List<Tag> = emptyList(),
-
+    // 사진 위 태그들
+    val productTagPinList : List<Tag> = emptyList(),
     // 저장됨 수
     val savedCount: Int = 0,
-
     // 댓글 수
     val commentCount: Int = 0,
-
     // 랭킹 정보 (1, 2, 3등) 추가
     val rank: Int? = null
 
