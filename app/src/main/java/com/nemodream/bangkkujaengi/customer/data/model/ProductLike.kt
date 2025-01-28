2ed7ba0c3e42ecce336d4de9af7c11e8eccf9d66
StopStone <wltjr6432@naver.com>
2025-01-28 21:46:39 +0900

    Refactor: 게시글 좋아요 확장함수 추가

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/data/model/ProductLike.kt
+++
@@ -1,7 +0,0 @@
-package com.nemodream.bangkkujaengi.customer.data.model
-
-data class ProductLike(
-    val userId: String = "",
-    val productIds: List<String> = emptyList(),
-    val updatedAt: Long = System.currentTimeMillis()
-)
