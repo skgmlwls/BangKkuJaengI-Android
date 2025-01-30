3f28a8517c40ebbf8f9c1d4eeab6bbfa43a61de1
hyeinLion <hyein604@naver.com>
2025-01-31 02:31:31 +0900

    Design: 게시글 상세 페이지 UI 디자인

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/fragment/SocialDetailFragment.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/fragment/SocialDetailFragment.kt
@@ -1,5 +1,7 @@
 package com.nemodream.bangkkujaengi.customer.ui.fragment
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +11,9 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nemodream.bangkkujaengi.R
+import com.nemodream.bangkkujaengi.customer.data.model.Member
 import com.nemodream.bangkkujaengi.databinding.FragmentSocialDetailBinding
+import com.nemodream.bangkkujaengi.utils.loadImage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +21,7 @@ class SocialDetailFragment : Fragment() {
 
     private var _binding: FragmentSocialDetailBinding? = null
     private val binding get() = _binding!!
+    private var isFollowing = false
 
     // private val viewModel: SocialFollowingAllViewModel by viewModels()
 
@@ -52,6 +57,47 @@ class SocialDetailFragment : Fragment() {
             toolbarSocial.setNavigationOnClickListener {
                 findNavController().popBackStack(R.id.navigation_social, false)
             }
+            // 초기 상태 설정 (isFollowing 여부에 따라)
+
+            updateButtonState(isFollowing)
+
+            // 버튼 클릭 리스너
+            btnFollowingFollowing.setOnClickListener {
+                isFollowing = !isFollowing
+                updateButtonState(isFollowing)
+
+//                if (isFollowing) {
+//                    // 팔로우 목록에 멤버 추가 (나중에 구현 예정)
+//                } else {
+//                    // 팔로우 목록에서 멤버 제거 (나중에 구현 예정)
+//                }
+            }
+
+
+
+        }
+    }
+    private fun updateButtonState(isFollowing: Boolean) {
+        if (isFollowing) {
+            binding.btnFollowingFollowing.text = "팔로잉"
+            binding.btnFollowingFollowing.setTextColor(Color.parseColor("#58443B")) // 글자색
+            binding.btnFollowingFollowing.setBackgroundTintList(
+                ColorStateList.valueOf(Color.parseColor("#DFDFDF")) // 버튼 배경색
+            )
+            binding.btnFollowingFollowing.strokeColor =
+                ColorStateList.valueOf(Color.parseColor("#818080")) // 테두리 색상
+            binding.btnFollowingFollowing.strokeWidth = 1 // 테두리 두께 (px 단위)
+            binding.btnFollowingFollowing.setPadding(0,0,0,0)
+        } else {
+            binding.btnFollowingFollowing.text = "팔로우"
+            binding.btnFollowingFollowing.setTextColor(Color.parseColor("#FFFFFF")) // 글자색
+            binding.btnFollowingFollowing.setBackgroundTintList(
+                ColorStateList.valueOf(Color.parseColor("#332828")) // 버튼 배경색
+            )
+            binding.btnFollowingFollowing.strokeColor =
+                ColorStateList.valueOf(Color.parseColor("#000000")) // 테두리 색상 (기본값, 변경 가능)
+            binding.btnFollowingFollowing.strokeWidth = 1 // 테두리 두께 (px 단위)
+            binding.btnFollowingFollowing.setPadding(0,0,0,0)
         }
     }
 }
