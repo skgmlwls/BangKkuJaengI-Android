feeb8b02ad3e0292c14f44cd0877bf96a42e37fc
StopStone <wltjr6432@naver.com>
2025-01-30 23:46:02 +0900

    Feat: 최근 본 상품 불러오기

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/ui/viewmodel/MyPageViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nemodream.bangkkujaengi.customer.data.model.Member
+import com.nemodream.bangkkujaengi.customer.data.model.Product
 import com.nemodream.bangkkujaengi.customer.data.repository.MyPageRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -19,6 +20,14 @@ class MyPageViewModel @Inject constructor(
     private val _memberInfo = MutableLiveData<Member>()
     val memberInfo: LiveData<Member> = _memberInfo
 
+    // 최근 본 상품
+    private val _recentProductList = MutableLiveData<List<Product>>(emptyList())
+    val recentProductList: LiveData<List<Product>> = _recentProductList
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+
     // 멤버 정보 가져오기
     fun getMemberInfo(memberDocId: String) = viewModelScope.launch {
         runCatching {
@@ -29,4 +38,23 @@ class MyPageViewModel @Inject constructor(
             it.printStackTrace()
         }
     }
+
+    // 최근 본 상품을 불러오는 코드
+    fun loadRecentProduct(userId: String) = viewModelScope.launch {
+        runCatching {
+            _isLoading.value = true
+            val products = repository.loadRecentProduct(userId)
+            products.map { product ->
+                val imageUrl = repository.getImageUrl(product.images.first())
+                product.copy(images = listOf(imageUrl))
+            }
+        }.onSuccess {
+            _recentProductList.value = it
+            _isLoading.value = false
+        }.onFailure {
+            it.printStackTrace()
+            _isLoading.value = false
+        }
+    }
+
 }
