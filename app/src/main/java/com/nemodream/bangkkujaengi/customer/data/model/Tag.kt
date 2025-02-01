86e1e387bb05486c3d5adfcc5051370659fc6556
hyeinLion <hyein604@naver.com>
2025-02-02 01:54:27 +0900

    주석 추가

--- app/src/main/java/com/nemodream/bangkkujaengi/customer/data/model/Tag.kt
+++ app/src/main/java/com/nemodream/bangkkujaengi/customer/data/model/Tag.kt
@@ -1,9 +1,8 @@
 package com.nemodream.bangkkujaengi.customer.data.model
 
-
 data class Tag(
-    val order: Int? = null,
-    val tagX: Float ? = null,
-    val tagY: Float ? = null,
-    val tagProductInfo: Product ? = null
+    val order: Int? = null, // 사진이 몇번째인지의 정보
+    val tagX: Float ? = null, // 태그핀의 x위치
+    val tagY: Float ? = null, // 태그핀의 y위치
+    val tagProductInfo: Product ? = null // 태그핀이 가지고 있는 태그 상품 정보
 )
